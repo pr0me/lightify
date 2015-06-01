96fbe12f148348f67d68b6348fae205d508bd6d0
pr0me <g33sus@gmail.com>
2015-06-01 13:32:32 +0200

    structure update

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-Ambilight with Hue
-==================
+Ambient Lighting with Hue
+=========================
 
 An ambilight approach using Philips Hue Light System and written in Python.
 
 ## Requirements
 
 * Python 3.4
-* optimally Phillips Hue Lights with a connecting Bridge
+* Phillips Hue Lights with a connecting Bridge
 * dependencies: phue, os and sys std libs, Pillow Implementation of PIL for Py3.X
 
-##Acknowledgements
+## Change Log
+* added pre-built c-/ cython-files, user is not required to have Cython installed
+* analysis and calculation functions are now cythonized for a 200% speedup
+* split up lightify (main) and calculation parts
+
+## Acknowledgements
 
 Thanks to https://github.com/studioimaginaire/phue for the awesome phue
 Python-library, and all the other developers of the used, very helpful
 libraries.
-
 
 "Hue Personal Wireless Lighting" is a trademark owned by Koninklijke Philips
 Electronics N.V., see www.meethue.com for more information. I am in no way
--- lightify.py
+++ lightify.py
@@ -6,6 +6,7 @@
 import sys, os
 from PIL import Image
 from phue import Bridge
+import analysisC
 
 IP = '192.168.178.31'
 
@@ -22,93 +23,24 @@ def connect():
             print("Please press the button on your bridge to connect!")
             connected = False
 
-
-def processImage():
-    os.system("import -window root screen.png")
-    # open the image
-    img = Image.open("screen.png")
-
-    # grab width and height
-    width, height = img.size
-
-    # make a list of all pixels in the image
-    pixels = img.load()
-    data = []
-    for x in range(width):
-        for y in range(height):
-            cpixel = pixels[x, y]
-            data.append(cpixel)
-    r = 0
-    g = 0
-    b = 0
-    counter = 0
-    # loop through all pixels
-    # if alpha value is greater than 200/255, add it to the average
-    # (note: could also use criteria like, if not a black pixel or not a white pixel...)
-    for x in range(len(data)):
-        try:
-            if int(data[x][3]) >= 200:
-                r += int(data[x][0])
-                g += int(data[x][1])
-                b += int(data[x][2])
-        except:
-            r += int(data[x][0])
-            g += int(data[x][1])
-            b += int(data[x][2])
-
-        counter += 1
-
-    # compute average RGB values
-    rAvg = r/counter
-    gAvg = g/counter
-    bAvg = b/counter
-    avgColor = [rAvg, gAvg, bAvg]
-    os.system("rm screen.png")
-    # print(str(avgColor[0]) + " " + str(avgColor[1]) + " " + str(avgColor[2]))
-    return avgColor
-
-
-def calcColor(r, g, b):
-    # converting RGB to xy
-    red = r / 255.0
-    green = g / 255.0
-    blue = b / 255.0
-    colorsRGB = gammaCorrection(red, green, blue)
-    red = colorsRGB[0]
-    green = colorsRGB[1]
-    blue = colorsRGB[2]
-    X = red * 0.649926 + green * 0.103455 + blue * 0.197109
-    Y = red * 0.234327 + green * 0.743075 + blue * 0.022598
-    Z = red * 0.0000000 + green * 0.053077 + blue * 1.035763
-    xF = X / (X + Y + Z)
-    yF = Y / (X + Y + Z)
-    colorsXY = [xF, yF, Y]
-    return colorsXY
-
-
-def gammaCorrection(r, g, b):
-    # simple Gamma Correction to match onscreen-colors
-    red = ((r + 0.055) / (1.0 + 0.055))**2.4 if (r > 0.04045) else (r / 12.92)
-    green = ((g + 0.055) / (1.0 + 0.055))**2.4 if (g > 0.04045) else (g / 12.92)
-    blue = ((b + 0.055) / (1.0 + 0.055))**2.4 if (b > 0.04045) else (b / 12.92)
-    colors = [red, green, blue]
-    return colors
-
-
 def main():
     connect()
+    b.set_light(1, 'on', True)
+    lights[0].brightness = 200
+    currColors = [0, 0]
 
     while True:
         try:
+            os.system("import -window root screen.png")
             avgColor = processImage()
-            colors = calcColor(avgColor[0], avgColor[1], avgColor[2])
-            b.set_light(1, 'on', True)
-            lights = b.get_light_objects()
-            # lights[0].brightness = int(colors[2])
-            lights[0].brightness = 200
-            lights[0].xy = [colors[0],colors[1]]
+            lastColors = currColors
+            currColors = calcColor(avgColor[0], avgColor[1], avgColor[2])
+            if (currColors[0] > lastColors[0] + 0.01 or currColors[0] < lastColors[0] - 0.01 or
+                currColors[1] > lastColors[1] + 0.01 or currColors[1] < lastColors[1] - 0.01):
+               lights = b.get_light_objects()
+               lights[0].xy = [currColors[0],currColors[1]]
         except:
+            lights = b.get_light_objects()
             lights[0].brightness = 200
 
 main()
-
